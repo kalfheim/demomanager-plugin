47419420f3960c66f4131b71a90650bd28f8e6ba
krisawzm <kristoffer.alfh@gmail.com>
2015-05-24 21:03:06 +0200

    UserCounter class

---
+++ classes/UserCounter.php
@@ -0,0 +1,63 @@
+<?php namespace Krisawzm\DemoManager\Classes;
+
+use October\Rain\Support\Traits\Singleton;
+use Cache;
+use Config;
+use File;
+
+class UserCounter
+{
+    use Singleton;
+
+    /**
+     * Cache key.
+     *
+     * @var string
+     */
+    protected $cacheKey = 'krisawzm-demomanager-usercounter';
+
+    /**
+     * Initialize.
+     *
+     * @return void
+     */
+    public function init()
+    {
+        if (!Cache::has($this->cacheKey)) {
+            Cache::forever(
+                $this->cacheKey,
+                count(File::directories($themesPath)) - 1
+            );
+        }
+    }
+
+    /**
+     * Get the number of demo users.
+     *
+     * @return int
+     */
+    public function get()
+    {
+        return Cache::get($this->cacheKey);
+    }
+
+    /**
+     * Increment the counter.
+     *
+     * @return int
+     */
+    public function inc()
+    {
+        return Cache::inc($this->cacheKey);
+    }
+
+    /**
+     * Returns true if the limit is reached.
+     *
+     * @return bool
+     */
+    public function limit()
+    {
+        return self::get() >= Config::get('krisawzm.demomanager::limit', 500);
+    }
+}
